431edee4950f6e0b5228dbb16fb13434b543d232
Ottavio Fogliata <5075650+ott-fogliata@users.noreply.github.com>
2021-10-26 16:29:57 +0200

    improvements on quotes

--- tests/test_sentence_tokenizer.py
+++ tests/test_sentence_tokenizer.py
@@ -3,7 +3,11 @@ from sentence_tokenizer import SentenceTokenizer
 
 s = SentenceTokenizer()
 
-s = s.set("If you're a plant lover: this guide is just for you. ##In fact, we're going to find out together how to take care of your plants at home even in the winter, with the thermostats on. Drought and temperature fluctuations can put your plants at risk for survival, but Attention to the thermometers.")
+s = s.set("If you're a plant lover: this guide is just for you. ##In fact, we're going to find out together how to "
+          "take care of your plants at home even in the winter, with the thermostats on. Drought and temperature "
+          "fluctuations can put your plants at risk for survival, but Attention to the thermometers. "
+          "Now what I'll write is some quotes: for example \"this is a quote, with us standards marks.\" But now the "
+          "sentence is blah blah: is it works? Also, this is a dot.With something attached.")
 
 
 print(s.get())
