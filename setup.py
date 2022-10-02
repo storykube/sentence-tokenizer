0583abd94f7934e722d814f74d34a14f3a7bc8d8
Ottavio Fogliata <5075650+ott-fogliata@users.noreply.github.com>
2022-10-02 19:14:08 +0200

    wrapping the pySBD library

--- setup.py
+++ setup.py
@@ -5,22 +5,12 @@ from setuptools.command.install import install
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-
-def installNLTKDependencies():
-    import nltk
-    nltk.download("gutenberg")
-    nltk.download("webtext")
-    nltk.download("punkt")
-    nltk.download("stopwords")
-
-
 setuptools.setup(
     name="sentence_tokenizer",
-    version="0.0.6",
+    version="0.1.0",
     author="Ottavio Fogliata",
     author_email="ottavio.fogliata@storykube.com",
-    description='Storykube Sentence Tokenizer. It splits the article text in sentences, following basic syntactic and '
-                'grammar rules from English Language.',
+    description='Storykube Sentence Tokenizer: split the article text in sentences with pySBD.',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/storykube/sentence_tokenizer",
@@ -31,13 +21,12 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
     ],
     install_requires=[
-        'nltk',
-        'spacy',
-        'langdetect'
+        'sumy',
+        'langdetect',
+        'pysbd==0.3.4'
     ],
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
 )
 
-installNLTKDependencies()
--- src/sentence_tokenizer/__init__.py
+++ src/sentence_tokenizer/__init__.py
@@ -1,31 +1,10 @@
 #!/usr/bin/python3
-import spacy
 import re
 import string
 
-import nltk
-from nltk.corpus import gutenberg, stopwords, webtext
-from nltk.tokenize.punkt import PunktSentenceTokenizer, PunktTrainer
 from sumy.nlp.tokenizers import Tokenizer
-
-from .abbrevs import Abbrevs
+import pysbd
 from langdetect import detect
-
-
-class SpacyStatic:
-    MAP = {
-        'it': 'it_core_news_sm',
-        'en': 'en_core_web_sm',
-        'es': 'es_core_news_sm',
-        'fr': 'fr_core_news_sm',
-        'de': 'de_core_news_sm',
-        'ru': 'ru_core_news_sm',
-    }
-
-    NLP_MEM = {}
-
-    def __init__(self):
-        pass
 
 
 class SingletonSentenceTokenizerContainer:
@@ -37,87 +16,20 @@ class SingletonSentenceTokenizerContainer:
 
 class SentenceTokenizer:
     """
-    The Storykube Sentence Tokenizer.
+    The Storykube Sentence Tokenizer / A wrapper of PySBD
+    https://github.com/nipunsadvilkar/pySBD
     """
 
-    DOT_REPLACE = "~~1~~"
-    ASK_REPLACE = "~~2~~"
-    EXC_REPLACE = "~~3~~"
-    DOT_DOT_REPLACE = "~~4~~"
-    COLON_REPLACE = "~~5~~"
-
     def __init__(self):
-
         pass
 
     def set(self, text):
-
         self.text = text
-
         return self
 
     def get(self) -> list:
-
-        def protect_chars_between_quotes(text_str):
-            text_str = text_str.replace('“', '"')
-            text_str = text_str.replace('”', '"')
-            text_str = re.sub(
-                r'\.\.\.(?!(?:[^"]*"[^"]*")*[^"]*$)', SentenceTokenizer.DOT_DOT_REPLACE, text_str)
-
-            text_str = re.sub(r'\.(?!(?:[^"]*"[^"]*")*[^"]*$)',
-                              SentenceTokenizer.DOT_REPLACE, text_str)
-
-            text_str = re.sub(r'\?(?!(?:[^"]*"[^"]*")*[^"]*$)',
-                              SentenceTokenizer.ASK_REPLACE, text_str)
-
-            text_str = re.sub(r'\!(?!(?:[^"]*"[^"]*")*[^"]*$)',
-                              SentenceTokenizer.EXC_REPLACE, text_str)
-
-            text_str = re.sub(r'\:(?!(?:[^"]*"[^"]*")*[^"]*$)',
-                              SentenceTokenizer.COLON_REPLACE, text_str)
-
-            return text_str
-
-        def restore_protected_chars(text_str):
-            text_str = text_str.replace(SentenceTokenizer.DOT_DOT_REPLACE, "...")
-            text_str = text_str.replace(SentenceTokenizer.DOT_REPLACE, ".")
-            text_str = text_str.replace(SentenceTokenizer.ASK_REPLACE, "?")
-            text_str = text_str.replace(SentenceTokenizer.EXC_REPLACE, "!")
-            text_str = text_str.replace(SentenceTokenizer.COLON_REPLACE, ":")
-
-            # restore doubled quotes
-            text_str = re.sub(r'(^|\s)\"', " “", text_str)
-            text_str = re.sub(r'\"\s', "” ", text_str)
-            text_str = re.sub(r'\"\.', "”.", text_str)
-            text_str = re.sub(r'\.\"', ".”", text_str)
-            text_str = re.sub(r'\"\,', "”,", text_str)
-            text_str = re.sub(r'\,\"', ",”", text_str)
-            text_str = re.sub(r'\"\-', "”-", text_str)
-            text_str = re.sub(r'\"\s\-', "” -", text_str)
-            text_str = re.sub(r'\"$', "”", text_str)
-            text_str = text_str.replace('"', '“')  # latest tentative, classic replace.
-
-            return text_str
-
-        lang_code = detect(self.text)
-
-        if lang_code not in SpacyStatic.NLP_MEM.keys():
-            if lang_code not in SpacyStatic.MAP:
-                lang_code = 'en'
-
-            SpacyStatic.NLP_MEM[lang_code] = \
-                spacy.load(SpacyStatic.MAP[lang_code])
-
-        text = protect_chars_between_quotes(self.text)
-
-        tokens = SpacyStatic.NLP_MEM[lang_code](text)
-
-        result = []
-
-        for sent in tokens.sents:
-            result.append(restore_protected_chars(str(sent).strip()))
-
-        return result
+        seg = pysbd.Segmenter(language=detect(self.text[:256]), clean=False)
+        return seg.segment(self.text)
 
     # these two methods will be used from the sumy package (mixer)
 
